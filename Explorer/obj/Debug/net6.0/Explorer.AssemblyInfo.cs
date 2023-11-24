ada81c389d55b0187b98f2932595a49c5086fd73
unskilled1 <89609602815.k@gmail.com>
2023-11-24 11:25:55 +0700

    Remove big files

--- Explorer/obj/Debug/net6.0/Explorer.AssemblyInfo.cs
+++ Explorer/obj/Debug/net6.0/Explorer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Explorer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2260744ff0531199e913967350e455b8bb25b1f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Explorer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Explorer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
